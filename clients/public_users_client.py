caffcdb2cfbf133a69fd111c0c05a0770b0598a0
Roman <savroful@gmail.com>
2025-05-26 11:25:03 +0300

    PublicUserClient impl

---
+++ clients/public_users_client.py
@@ -0,0 +1,31 @@
+from typing import TypedDict
+
+from httpx import Response
+
+from .api_client import APIClient
+
+
+class CreateUserDict(TypedDict):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+
+class PublicUsersClient(APIClient):
+    """
+    Клиент для работы с /api/v1/users
+    """
+    
+    def create_user_api(self request: CreateUserDict) -> Response:
+        """
+        Метод выполняет создание пользователя.
+
+        :param request: Словарь с данными для создания пользователя.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.post("/api/v1/users", json=request)
