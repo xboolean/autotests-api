a8f224c2ff16fcaf288b3cd7ea57f05c6ee00910
Roman <savroful@gmail.com>
2025-05-26 18:30:25 +0300

    pydantic models impl

---
+++ pydantic_create_user.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field, EmailStr
+
+
+class UserSchema(BaseModel):
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+
+class CreateUserRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+
+class CreateUserResponseSchema(BaseModel):
+    user: UserSchema
